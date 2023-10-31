a3a919c24a1e68ba16013860e8eae66cc0436c10
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-10-31 16:07:04 +0100

    chore: filter load resource by same origin

--- src/index.js
+++ src/index.js
@@ -139,6 +139,7 @@ if (window.location.hostname === 'blog.adobe.com') {
   const observer = new PerformanceObserver((list) => {
     list.getEntries()
       .filter((entry) => !entry.responseStatus || entry.responseStatus < 400)
+      .filter((entry) => window.location.hostname === new URL(entry.name).hostname)
       .filter((entry) => new URL(entry.name).pathname.match('.*(\\.plain\\.html|\\.json)$'))
       .forEach((entry) => {
         sampleRUM('loadresource', { source: entry.name, target: Math.round(entry.duration) });
