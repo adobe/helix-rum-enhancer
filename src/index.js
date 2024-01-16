c049f930f0cc1dc548e42bd8e76cd97709a873d7
Lars Trieloff <lars@trieloff.net>
2024-01-16 17:45:30 +0100

    feat(observer): enable tracking of JSON loading for all browsers that have a PerformanceObserver implementation

--- src/index.js
+++ src/index.js
@@ -155,15 +155,13 @@ document.querySelectorAll('form').forEach((form) => {
 window.addEventListener('visibilitychange', ((event) => sampleRUM.leave(event)));
 window.addEventListener('pagehide', ((event) => sampleRUM.leave(event)));
 
-if (window.location.hostname === 'blog.adobe.com') {
-  const observer = new PerformanceObserver((list) => {
-    list.getEntries()
-      .filter((entry) => !entry.responseStatus || entry.responseStatus < 400)
-      .filter((entry) => window.location.hostname === new URL(entry.name).hostname)
-      .filter((entry) => new URL(entry.name).pathname.match('.*(\\.plain\\.html|\\.json)$'))
-      .forEach((entry) => {
-        sampleRUM('loadresource', { source: entry.name, target: Math.round(entry.duration) });
-      });
-  });
-  observer.observe({ type: 'resource', buffered: true });
-}
+const observer = new PerformanceObserver((list) => {
+  list.getEntries()
+    .filter((entry) => !entry.responseStatus || entry.responseStatus < 400)
+    .filter((entry) => window.location.hostname === new URL(entry.name).hostname)
+    .filter((entry) => new URL(entry.name).pathname.match('.*(\\.plain\\.html|\\.json)$'))
+    .forEach((entry) => {
+      sampleRUM('loadresource', { source: entry.name, target: Math.round(entry.duration) });
+    });
+});
+observer.observe({ type: 'resource', buffered: true });
