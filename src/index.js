45b5e6428fb9c51e8fc4d477c574d44a361b3ec2
Lars Trieloff <lars@trieloff.net>
2024-01-16 17:50:19 +0100

    feat(observer): enable tracking of JSON loading for all browsers that have a PerformanceObserver implementation

--- src/index.js
+++ src/index.js
@@ -67,10 +67,6 @@ function optedIn(checkpoint, data) {
 }
 // Gets configured collection from the config service for the current domain
 function getCollectionConfig() {
-  if (window.location.hostname === 'blog.adobe.com') {
-    return ['loadresource', ...DEFAULT_TRACKING_EVENTS];
-  }
-  // TODO: configured collection should come from config service
   return DEFAULT_TRACKING_EVENTS;
 }
 
@@ -157,18 +153,16 @@ function addEnterLeaveTracking() {
 }
 
 function addLoadResourceTracking() {
-  if (window.location.hostname === 'blog.adobe.com') {
-    const observer = new PerformanceObserver((list) => {
-      list.getEntries()
-        .filter((entry) => !entry.responseStatus || entry.responseStatus < 400)
-        .filter((entry) => window.location.hostname === new URL(entry.name).hostname)
-        .filter((entry) => new URL(entry.name).pathname.match('.*(\\.plain\\.html|\\.json)$'))
-        .forEach((entry) => {
-          sampleRUM('loadresource', { source: entry.name, target: Math.round(entry.duration) });
-        });
-    });
-    observer.observe({ type: 'resource', buffered: true });
-  }
+  const observer = new PerformanceObserver((list) => {
+    list.getEntries()
+      .filter((entry) => !entry.responseStatus || entry.responseStatus < 400)
+      .filter((entry) => window.location.hostname === new URL(entry.name).hostname)
+      .filter((entry) => new URL(entry.name).pathname.match('.*(\\.plain\\.html|\\.json)$'))
+      .forEach((entry) => {
+        sampleRUM('loadresource', { source: entry.name, target: Math.round(entry.duration) });
+      });
+  });
+  observer.observe({ type: 'resource', buffered: true });
 }
 
 function activateBlocksMutationObserver() {
