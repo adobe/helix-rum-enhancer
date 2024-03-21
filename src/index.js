a5c16201d5cfc30dfe33014d543f2bbfde103bd3
Julien Ramboz <ramboz@adobe.com>
2024-03-21 09:46:10 -0700

    feat: track UTM query parameters

--- src/index.js
+++ src/index.js
@@ -177,3 +177,10 @@ const observer = new PerformanceObserver((list) => {
   }
 });
 observer.observe({ type: 'resource', buffered: true });
+
+const usp = new URLSearchParams(window.location.search);
+const utmParams = [...usp.entries()]
+  .filter(([key]) => key.startsWith('utm_') && key !== 'utm_id');
+utmParams.forEach(([key, value]) => {
+  sampleRUM('utm', { source: key, target: value });
+});
