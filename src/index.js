826e81a44af52b2579607e796bc9a5c9c4fe8f99
Alexandre Capt <acapt@adobe.com>
2024-06-03 10:28:31 +0200

    fix: avoid CORS

--- src/index.js
+++ src/index.js
@@ -111,8 +111,7 @@ function trackCheckpoint(checkpoint, data, t) {
       // eslint-disable-next-line object-curly-newline, max-len
       const body = JSON.stringify({ weight, id, sanitizeURL: urlSanitizers[window.hlx.RUM_MASK_URL || 'path'], checkpoint, t, ...data }, KNOWN_PROPERTIES);
       const url = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL || sampleRUM.baseURL).href;
-      const headers = { type: 'application/json' };
-      navigator.sendBeacon(url, new Blob([body], headers));
+      navigator.sendBeacon(url, body);
       // eslint-disable-next-line no-console
       console.debug(`ping:${checkpoint}`, pdata);
     };
