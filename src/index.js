0b1ecf1b91d8a4ab5fbfd0d49f2398e9827e5d45
Alexandre Capt <acapt@adobe.com>
2024-06-03 10:24:23 +0200

    fix: no this in arrow functions

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ const DEFAULT_TRACKING_EVENTS = ['click', 'cwv', 'form', 'enterleave', 'viewbloc
 const { sampleRUM, queue, isSelected } = (window.hlx && window.hlx.rum) ? window.hlx.rum : {};
 
 const fflags = {
-  has: (flag) => this[flag].indexOf(Array.from(window.origin)
+  has: (flag) => fflags[flag].indexOf(Array.from(window.origin)
     .map((a) => a.charCodeAt(0))
     .reduce((a, b) => a + b, 1) % 1371) !== -1,
-  enabled: (flag, callback) => this.has(flag) && callback(),
-  disabled: (flag, callback) => !this.has(flag) && callback(),
+  enabled: (flag, callback) => fflags.has(flag) && callback(),
+  disabled: (flag, callback) => !fflags.has(flag) && callback(),
   onetrust: [543, 770, 1136],
   ads: [1339],
   email: [1339],
