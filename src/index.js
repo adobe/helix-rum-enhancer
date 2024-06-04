4daf3903247b0834b6e4fd15a3ad3738ad6917b5
Alexandre Capt <acapt@adobe.com>
2024-06-04 10:29:08 +0200

    fix: avoid CORS (#183)
    * fix: avoid CORS
    
    * fix: check for H5

--- src/index.js
+++ src/index.js
@@ -110,9 +110,13 @@ function trackCheckpoint(checkpoint, data, t) {
     const sendPing = (pdata = data) => {
       // eslint-disable-next-line object-curly-newline, max-len
       const body = JSON.stringify({ weight, id, sanitizeURL: urlSanitizers[window.hlx.RUM_MASK_URL || 'path'], checkpoint, t, ...data }, KNOWN_PROPERTIES);
-      const url = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL || sampleRUM.baseURL).href;
-      const headers = { type: 'application/json' };
-      navigator.sendBeacon(url, new Blob([body], headers));
+      const { href: url, origin} = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL || sampleRUM.baseURL);
+      if (window.location.origin === origin) {
+        const headers = { type: 'application/json' };
+        navigator.sendBeacon(url, new Blob([body], headers));
+      } else {
+        navigator.sendBeacon(url, body);
+      }
       // eslint-disable-next-line no-console
       console.debug(`ping:${checkpoint}`, pdata);
     };
