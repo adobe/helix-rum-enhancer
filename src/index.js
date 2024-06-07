1b9f16839cd6e9389b96018261391f1e824cc109
Lars Trieloff <lars@trieloff.net>
2024-06-07 11:50:35 +0200

    feat(ads): enable ads feature flag

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const fflags = {
   enabled: (flag, callback) => fflags.has(flag) && callback(),
   disabled: (flag, callback) => !fflags.has(flag) && callback(),
   onetrust: [543, 770, 1136],
-  ads: [1339],
+  ads: [1339, 1198],
   email: [1339],
 };
 
