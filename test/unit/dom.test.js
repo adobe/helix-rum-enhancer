adb8f7112343a824ad54f8de9517464c78a771c0
Alexandre Capt <acapt@adobe.com>
2024-06-10 17:28:01 +0200

    chore: targetselector tests

--- test/unit/dom.test.js
+++ test/unit/dom.test.js
@@ -13,10 +13,10 @@
 /* eslint-env mocha */
 
 import { expect } from '@esm-bundle/chai';
-import { getTargetValue } from '../../src/dom.js';
+import { getTargetValue, targetselector } from '../../src/dom.js';
 
 describe('test dom#getTargetValue', () => {
-  it('getTargetValue basics', () => {
+  it('getTargetValue - basics', () => {
     expect(getTargetValue).to.be.a('function');
   });
 
@@ -28,31 +28,76 @@ describe('test dom#getTargetValue', () => {
 
   it('getTargetValue - select href attr', () => {
     const a = document.createElement('a');
-    a.setAttribute('href', 'http://www.example.com');
-    expect(getTargetValue(a)).to.be.equal('http://www.example.com');
+    a.setAttribute('href', 'https://www.example.com');
+    expect(getTargetValue(a)).to.be.equal('https://www.example.com');
   });
 
   it('getTargetValue - select currentSrc attr', () => {
     const img = document.createElement('img');
-    img.src = 'http://www.example.com/img.jpg';
-    expect(getTargetValue(img)).to.be.equal('http://www.example.com/img.jpg');
+    img.src = 'https://www.example.com/img.jpg';
+    expect(getTargetValue(img)).to.be.equal('https://www.example.com/img.jpg');
   });
 
   it('getTargetValue - select src attr', () => {
     const img = document.createElement('img');
-    img.setAttribute('src', 'http://www.example.com/img.jpg');
-    expect(getTargetValue(img)).to.be.equal('http://www.example.com/img.jpg');
+    img.setAttribute('src', 'https://www.example.com/img.jpg');
+    expect(getTargetValue(img)).to.be.equal('https://www.example.com/img.jpg');
   });
 
   it('getTargetValue - select data-action attr', () => {
     const form = document.createElement('form');
-    form.setAttribute('data-action', 'http://www.example.com/action');
-    expect(getTargetValue(form)).to.be.equal('http://www.example.com/action');
+    form.setAttribute('data-action', 'https://www.example.com/action');
+    expect(getTargetValue(form)).to.be.equal('https://www.example.com/action');
   });
 
   it('getTargetValue - select action attr', () => {
     const form = document.createElement('form');
-    form.setAttribute('action', 'http://www.example.com/action');
-    expect(getTargetValue(form)).to.be.equal('http://www.example.com/action');
+    form.setAttribute('action', 'https://www.example.com/action');
+    expect(getTargetValue(form)).to.be.equal('https://www.example.com/action');
+  });
+});
+
+describe('test dom#targetselector', () => {
+  it('targetselector - basics', () => {
+    expect(targetselector).to.be.a('function');
+    // eslint-disable-next-line no-unused-expressions
+    expect(targetselector()).to.be.undefined;
+  });
+
+  it('targetselector - select target for link', () => {
+    const a = document.createElement('a');
+    a.setAttribute('href', 'https://www.example.com');
+    expect(targetselector(a)).to.be.equal('https://www.example.com');
+  });
+
+  it('targetselector - select target for relative link', () => {
+    const a = document.createElement('a');
+    a.setAttribute('href', '/target.html');
+    expect(targetselector(a)).to.be.equal('http://localhost:8000/target.html');
+  });
+
+  it('targetselector - select target for span in a link', () => {
+    const a = document.createElement('a');
+    a.setAttribute('href', 'https://www.example.com/target.html');
+    const span = document.createElement('span');
+    span.textContent = 'test';
+    a.append(span);
+    expect(targetselector(span)).to.be.equal('https://www.example.com/target.html');
+  });
+
+  it('targetselector - select target for span with data-rum-target in a link', () => {
+    const a = document.createElement('a');
+    a.setAttribute('href', 'https://www.example.com/target.html');
+    const span = document.createElement('span');
+    span.textContent = 'test';
+    span.setAttribute('data-rum-target', 'test');
+    a.append(span);
+    expect(targetselector(span)).to.be.equal('http://localhost:8000/test');
+  });
+
+  it('targetselector - select target for img', () => {
+    const img = document.createElement('img');
+    img.src = 'https://www.example.com/img.jpg';
+    expect(targetselector(img)).to.be.equal('https://www.example.com/img.jpg');
   });
 });
