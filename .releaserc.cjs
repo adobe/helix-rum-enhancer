ead617d97ec3341b5ec887d1e9471c937d032eff
David Bosschaert <bosschae@adobe.com>
2024-09-17 16:11:28 +0200

    feat(release) Generate checksum of the rum enhancer on release
    Fixed: #282

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,7 +10,12 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {
+      "assets": [
+        { "path": "src/index.js", "label": "JS distribution" },
+        { "path": "src/checksum.sha256", "label": "Checksum" }
+      ]
+    }]
   ],
-  branches: ['main'],
+  branches: ['main', 'test-release'],
 };
