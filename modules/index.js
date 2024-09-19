02f4d0b366505fc17bc251e29364efa2e497c299
Francisco Chicharro Sanz <chicharr@adobe.com>
2024-09-19 14:25:42 +0200

    feat(index): support  to attach custom URL parameters to collection URL

--- modules/index.js
+++ modules/index.js
@@ -41,7 +41,8 @@ function trackCheckpoint(checkpoint, data, t) {
     const sendPing = (pdata = data) => {
       // eslint-disable-next-line object-curly-newline, max-len
       const body = JSON.stringify({ weight, id, referer: urlSanitizers[window.hlx.RUM_MASK_URL || 'path'](), checkpoint, t, ...data }, KNOWN_PROPERTIES);
-      const { href: url, origin } = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL || sampleRUM.baseURL);
+      const urlParams = window.RUM_PARAMS ? `?${new URLSearchParams(window.RUM_PARAMS).toString()}` : '';
+      const { href: url, origin } = new URL(`.rum/${weight}${urlParams}`, sampleRUM.collectBaseURL);
       if (window.location.origin === origin) {
         const headers = { type: 'application/json' };
         navigator.sendBeacon(url, new Blob([body], headers));
