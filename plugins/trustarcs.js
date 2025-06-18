1023224071ca9540bef60c75c2aced7aa2f1695c
Christopher Wisse <cwisse@adobe.com>
2025-06-18 18:05:42 +0200

    feat(consent): add support for UserCentrics and TrustArcs consent providers

--- plugins/trustarcs.js
+++ plugins/trustarcs.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Adobe. All rights reserved.
+ * Copyright 2025 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,28 +9,20 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-function sampleOneTrust() {
-  const cmpCookie = document.cookie.split(';')
-    .map((c) => c.trim())
-    .find((cookie) => cookie.startsWith('OptanonAlertBoxClosed='));
+function sampleTrustArcs() {
+  const taConsentTrack = document.querySelector('#truste-consent-track');
 
-  if (cmpCookie) {
-    return { source: 'onetrust', target: 'hidden' };
+  if (taConsentTrack && taConsentTrack.offsetHeight > 0) {
+    return { source: 'trustarcs', target: 'show' };
   }
 
-  const bannersdk = document.querySelector('#onetrust-banner-sdk');
+  const taConsentContent = document.querySelector('#truste-consent-content');
 
-  if (bannersdk && bannersdk.offsetHeight > 0) {
-    return { source: 'onetrust', target: 'show' };
+  if (taConsentContent && taConsentContent.offsetHeight > 0) {
+    return { source: 'trustarcs', target: 'show' };
   }
 
-  const pcsdk = document.querySelector('#onetrust-pc-sdk');
-
-  if (pcsdk && pcsdk.offsetHeight > 0) {
-    return { source: 'onetrust', target: 'show' };
-  }
-
-  return { source: 'onetrust', target: 'suppressed' };
+  return { source: 'trustarcs', target: 'suppressed' };
 }
 
 let hasSentData = false;
@@ -39,6 +31,6 @@ export default function addCookieConsentTracking({ sampleRUM }) {
     return;
   }
 
-  sampleRUM('consent', sampleOneTrust());
+  sampleRUM('consent', sampleTrustArcs());
   hasSentData = true;
 }
